367745435896b9e734c64741c081a84c26de70c6
Sergio Fidelis <sergiofidelis31@hotmail.com>
2022-12-26 23:17:18 -0300

    add post login implementation

--- users/src/main/java/com/springbootapirestcourse/users/security/WebSecurity.java
+++ users/src/main/java/com/springbootapirestcourse/users/security/WebSecurity.java
@@ -3,39 +3,53 @@ package com.springbootapirestcourse.users.security;
 
 import com.springbootapirestcourse.users.service.UserService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
+@Configuration
 public class WebSecurity {
     private final UserService userService;
-
     private final BCryptPasswordEncoder passwordEncoder;
+    private final AuthenticationConfiguration authenticationConfiguration;
 
-    public WebSecurity(UserService userService, BCryptPasswordEncoder passwordEncoder) {
+    public WebSecurity(UserService userService, BCryptPasswordEncoder passwordEncoder, AuthenticationConfiguration authenticationConfiguration) {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
+        this.authenticationConfiguration = authenticationConfiguration;
     }
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception{
         AuthenticationManagerBuilder authenticationManagerBuilder =
                 http.getSharedObject(AuthenticationManagerBuilder.class);
+
         authenticationManagerBuilder
                 .userDetailsService(userService)
                 .passwordEncoder(passwordEncoder);
+
         http
                 .csrf()
                 .disable()
-                .authorizeHttpRequests()
-                .requestMatchers(HttpMethod.POST, "/users")
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
                 .permitAll()
                 .anyRequest()
-                .authenticated();
+                .authenticated()
+                .and()
+                .addFilter(new AuthenticationFilter(authenticationManager()));
+
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
--- users/src/main/java/com/springbootapirestcourse/users/service/implementation/UserServiceImplementation.java
+++ users/src/main/java/com/springbootapirestcourse/users/service/implementation/UserServiceImplementation.java
@@ -7,10 +7,13 @@ import com.springbootapirestcourse.users.shared.Utils;
 import com.springbootapirestcourse.users.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class UserServiceImplementation implements UserService {
@@ -37,7 +40,13 @@ public class UserServiceImplementation implements UserService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity userEntity = this.userRepository.findByEmail(email);
+
+        if (userEntity == null ){
+            throw new UsernameNotFoundException(email);
+        }
+
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
     }
 }
